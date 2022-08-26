9b7ac07f8981d99af8497f79fc61557f28d29671
Clarence Silva Aguiar <clarenceaguiar@gmail.com>
2022-08-26 16:18:59 -0300

    atribuição de valores às variáveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let seuEmail
 let suaProfissao
 let suaIdade
 let lugarQueSonhaConhecer
+seuNome = "clarence"
+seuEmail = "clarenceaguiar@gmail.com"
+suaProfissao = "Administrador"
+suaIdade = 39
+lugarQueSonhaConhecer = "Porto"
